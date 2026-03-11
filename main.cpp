eedfa0007e38a56c57fa4a03752f1ea4098bf6b9
OvidiuT11 <ovidiutecu99@gmail.com>
2026-03-11 21:15:05 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "uesavegame.h"
+
+int main() {
+    SaveGame save;
+
+    if(!save.load("Profile.sav")) {
+        std::cout << "Nu s-a putut incarca save-ul\n";
+        return 1;
+    }
+
+    std::cout << "Save incarcat\n";
+
+    save.save("Profile_copy.sav");
+
+    std::cout << "Save rescris\n";
+
+    return 0;
+}
